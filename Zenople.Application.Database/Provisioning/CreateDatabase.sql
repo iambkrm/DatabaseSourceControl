a66758596d8dc00396b49de5a64c98ede94577ca
Bikram Neupane <bikram.neupane@aqore.com>
2022-04-28 20:06:58 +0545

    commit changes

--- Zenople.Application.Database/Provisioning/CreateDatabase.sql
+++
@@ -1,7 +0,0 @@
-PRINT N'CUSTOM: Creating database [$(DatabaseName)]...';
-GO
-IF SERVERPROPERTY('EngineEdition') = 5
-  EXEC sp_executesql N'CREATE DATABASE [$(DatabaseName)] (EDITION = ''Basic'');';
-ELSE
-  CREATE DATABASE [$(DatabaseName)];
-GO
